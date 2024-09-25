eb08fab36da91d8990a3e505e80b1e88daf947e5
morkato <the.morkato@outlook.com>
2024-09-25 15:08:23 -0300

    refactor(attacks): move table "attacks" for this object (tables)

--- src/main/kotlin/morkato/api/database/tables.kt
+++ src/main/kotlin/morkato/api/database/tables.kt
@@ -31,4 +31,21 @@ object tables {
 
     override val primaryKey = PrimaryKey(guild_id, id)
   }
+
+  object attacks : Table("attacks") {
+    val guild_id = reference("guild_id", guilds.id)
+    val id = long("id").autoIncrement()
+
+    val name = varchar("name", length = 30)
+    val art_id = long("art_id")
+    val name_prefix_art = varchar("name_prefix_art", length = 32).nullable()
+    val description = varchar("description", length = 2048).nullable()
+    val banner = text("banner").nullable()
+    val damage = long("damage")
+    val breath = long("breath")
+    val blood = long("blood")
+    val intents = integer("intents")
+
+    override val primaryKey = PrimaryKey(guild_id, id)
+  }
 }
