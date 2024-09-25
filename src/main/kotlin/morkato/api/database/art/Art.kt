408b2c053d8b6ca2607ef8ae9cfac773e7baa62d
morkato <the.morkato@outlook.com>
2024-09-25 14:53:36 -0300

    refactor: refactor .Art class

--- src/main/kotlin/morkato/api/database/art/Art.kt
+++ src/main/kotlin/morkato/api/database/art/Art.kt
@@ -1,20 +1,17 @@
 package morkato.api.database.art
 
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import morkato.api.database.tables.arts
 import morkato.api.database.guild.Guild
 import org.jetbrains.exposed.sql.*
 
 class Art(
-  val guildId: String,
-  val id: Long,
-  val name: String,
-  val type: ArtType,
-  val description: String?,
-  val banner: String?
+  val guild: Guild,
+  val payload: ArtPayload
 ) {
   companion object {
-    fun from(row: ResultRow) : Art {
-      return Art(
+    fun getPayload(row: ResultRow) : ArtPayload {
+      return ArtPayload(
         row[arts.guild_id],
         row[arts.id],
         row[arts.name],
@@ -23,60 +20,10 @@ class Art(
         row[arts.banner]
       )
     }
-    object arts : Table("arts") {
-      val guild_id = varchar("guild_id", length = 30)
-      val id = long("id").autoIncrement()
-
-      val name = varchar("name", length = 32)
-      val type = enumerationByName<ArtType>("type", length = 16, klass = ArtType::class)
-      val description = varchar("description", length = 2048).nullable()
-      val banner = text("banner").nullable()
-
-      override val primaryKey = PrimaryKey(guild_id, id)
-    }
-
-    fun findAllByGuildId(guildId: String) : List<Art> {
-      return arts
-        .selectAll()
-        .where(arts.guild_id eq guildId)
-        .map { from(it) }
-        .toList()
-    }
-
-    fun getReference(guildId: String, id: Long) : Art {
-      return from(
-        arts
-          .selectAll()
-          .where(arts.guild_id eq guildId)
-          .andWhere { arts.id eq id }
-          .single()
-      )
-    }
-
-    fun create(data: ArtCreateData, guild: Guild) : Art {
-      val id = arts.insert {
-        it[arts.guild_id] = guild.id;
-        it[arts.name] = data.name;
-        it[arts.type] = data.type;
-        it[arts.description] = data.description;
-        it[arts.banner] = data.banner;
-      } get arts.id
-      return Art(guild.id, id, data.name, data.type, data.description, data.banner)
-    }
-  }
-
-  fun copy(data: ArtUpdateData) : Art {
-    return Art(
-      guildId, id,
-      data.name ?: this.name,
-      data.type ?: this.type,
-      data.description ?: this.description,
-      data.banner ?: this.banner
-    )
   }
 
   fun update(data: ArtUpdateData) : Art {
-    arts.update({ (arts.guild_id eq guildId) and (arts.id eq id) }) {
+    arts.update({ (arts.guild_id eq guild.id) and (arts.id eq payload.id) }) {
       if (data.name != null) {
         it[arts.name] = data.name
       }
@@ -89,11 +36,14 @@ class Art(
       if (data.banner != null) {
         it[arts.banner] = data.banner
       }
-    };
-    return copy(data)
+    }
+    return Art(guild, payload.extend(data))
   }
   fun delete() : Art {
-    arts.deleteWhere { (arts.guild_id eq guildId) and (arts.id eq id) };
+    arts.deleteWhere {
+      (arts.guild_id eq guild.id)
+        .and(arts.id eq payload.id)
+    }
     return this;
   }
 }
