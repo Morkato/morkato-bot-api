6db9896038bacbd20517fa13f6d7397194bc79a3
morkato <the.morkato@outlook.com>
2024-09-26 08:47:25 -0300

    refactor(npcs): move table "npcs" for this object (tables)

--- src/main/kotlin/morkato/api/database/tables.kt
+++ src/main/kotlin/morkato/api/database/tables.kt
@@ -2,6 +2,7 @@ package morkato.api.database
 
 import morkato.api.database.ability.AbilityType
 import morkato.api.database.art.ArtType
+import morkato.api.database.npc.NpcType
 import org.jetbrains.exposed.sql.Table
 
 object tables {
@@ -80,5 +81,25 @@ object tables {
     val family_id = reference("family_id", families.id)
 
     override val primaryKey = PrimaryKey(guild_id, ability_id, family_id)
+  }
+
+  object npcs : Table("npcs") {
+    val guild_id = reference("guild_id", guilds.id)
+    val id = long("id").autoIncrement()
+
+    val name = varchar("name", length = 32)
+    val type = enumerationByName<NpcType>("type", length = 16, klass = NpcType::class)
+    val family_id = long("family_id")
+    val surname = varchar("surname", length = 32)
+    val energy = integer("energy")
+    val max_life = long("max_life")
+    val max_breath = long("max_breath")
+    val max_blood = long("max_blood")
+    val current_life = long("current_life")
+    val current_breath = long("current_breath")
+    val current_blood = long("current_blood")
+    val icon = text("icon").nullable()
+
+    override val primaryKey = PrimaryKey(guild_id, id)
   }
 }
